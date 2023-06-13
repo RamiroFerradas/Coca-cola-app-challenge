5515bd3cd6f3fe366ecb98803e6314dc9ef0a601
RamiroFerradas <ramiferra97@gmail.com>
2023-06-13 09:43:46 -0300

    add headers

--- app/services/clients.ts
+++ app/services/clients.ts
@@ -1,10 +1,16 @@
 import { Client } from "../models/Client";
 
 const URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
-
+console.log(URL);
 export async function getClients(): Promise<Client[]> {
   try {
-    const response = await fetch(`${URL}/clients`);
+    const response = await fetch(`${URL}/clients`, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
     if (!response.ok) {
       throw new Error("Error fetching clients");
     }
