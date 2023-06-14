84ee87f0349802cfefee051a8cf6850eab697285
RamiroFerradas <ramiferra97@gmail.com>
2023-06-14 10:33:29 -0300

    bug camera qr

--- app/login/components/QrScann/QrScann.tsx
+++ app/login/components/QrScann/QrScann.tsx
@@ -1,5 +1,5 @@
 import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import QrReader from "react-qr-scanner";
 
 type ValidateUserFunction = (code: number) => void;
@@ -30,6 +30,7 @@ const QrScann: React.FC<QrScannProps> = ({ validateUser }) => {
     justifyContent: "center",
     alignItems: "center",
   };
+  const qrRef = useRef(null);
   return !scan ? (
     <div
       className="flex flex-col justify-center items-center gap-3"
@@ -41,13 +42,16 @@ const QrScann: React.FC<QrScannProps> = ({ validateUser }) => {
   ) : (
     <div className="absolute">
       <QrReader
+        ref={qrRef}
         delay={100}
         style={previewStyle}
         onError={handleError}
         onScan={handleScan}
-        constraints={{
-          facingMode: "environment",
-        }}
+        // constraints={{
+        //   facingMode: "environment",
+        // }}
+        facingMode="environment"
+        // style={{ width: "100%" }}
       />
     </div>
   );
