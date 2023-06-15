096b47cd0fa3bb938e48e28e9591941999e4bff7
RamiroFerradas <ramiferra97@gmail.com>
2023-06-14 21:31:08 -0300

    fix bug

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,9 @@
-export { default as Appbar } from "./Appbar";
-export { default as Loader } from "./Loader";
-export { default as Navbar } from "./Navbar";
-export { default as Searchbar } from "./Searchbar";
+// export { default as Appbar_ } from "./Appbar_";
+// export { default as Loader_ } from "./Loader_";
+// export { default as Navbar_ } from "./Navbar_";
+// export { default as Searchbar_ } from "./Searchbar_";
 export * from "./index";
+export { default as Appbar } from "./appbar";
+export { default as Loader } from "./loader";
+export { default as Navbar } from "./navbar";
+export { default as Searchbar } from "./searchbar";
