68f79f5fc33b2afb5fa429bfb035560ed658accb
RamiroFerradas <ramiferra97@gmail.com>
2023-06-16 02:08:55 -0300

    onlySmarthpones

--- app/layoutPages.tsx
+++ app/layoutPages.tsx
@@ -3,8 +3,16 @@ import { Typography } from "@mui/material";
 import { useScreen } from "./hooks";
 import { Appbar, Loader, Navbar } from "./components";
 import { ThemeProvider, useTheme } from "./context/themeContext";
+import Image from "next/image";
+import onlySmartPhone from "@/public/assets/onlysmarthpone.jpg";
 // import "@fontsource/roboto/300.css";
 
+import { Oxygen } from "next/font/google";
+
+const inter = Oxygen({
+  weight: "700",
+  subsets: ["latin", "latin-ext"],
+});
 function LayoutPages({ children }: { children: React.ReactNode }) {
   const { mobileScreen, loaderScreen } = useScreen();
   const { theme } = useTheme();
@@ -20,10 +28,15 @@ function LayoutPages({ children }: { children: React.ReactNode }) {
           <Appbar />
         </div>
       ) : (
-        <div className="h-screen bg-red-200 flex justify-center items-center">
-          <Typography variant="h4" gutterBottom>
-            Versión exclusiva para celulares
-          </Typography>
+        <div className="h-screen flex justify around items-center relative w-screen">
+          <div className="w-1/2 p-20">
+            <p className={`font-bold text-6xl font ${inter.className}`}>
+              ¡App disponible únicamente para SmartPhones!
+            </p>
+          </div>
+          <div className="absolute right-0 bottom-0">
+            <Image src={onlySmartPhone} alt="onlysmartphone" />
+          </div>
         </div>
       )}
     </main>
