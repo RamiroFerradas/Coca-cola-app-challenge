84d15318c722527b27eec441de6eedbf8e77ddee
RamiroFerradas <ramiferra97@gmail.com>
2023-06-15 21:15:10 -0300

    bugfix screen mobile

--- app/hooks/useScreen.tsx
+++ app/hooks/useScreen.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function useScreen() {
-  const [mobileScreen, setMobileScreen] = useState<boolean>(true);
+  const [mobileScreen, setMobileScreen] = useState<boolean>(false);
 
   const handleResize = () => {
     const isMobile = window.innerWidth < 1200;
